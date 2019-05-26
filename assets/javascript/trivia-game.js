e0b0b58bc01077505ab921aca96cd8e2f3702c4e
Madeleine Kemeny <madeleinekemeny@gmail.com>
2019-05-26 14:05:41 -0500

    99 percent working; one problem with scoring

--- assets/javascript/trivia-game.js
+++ assets/javascript/trivia-game.js
@@ -5,10 +5,10 @@ var indexOfmyquest = 0;
 var possibleAnswers;
 var correctAnswers = 0;
 var incorrectGuesses = 0;
-var skipped = 0;
+var unansweredQ = 0;
+var isUnAnsweredQIncrementOneAtATime=true;
 
-
-//when game is opened, these buttons are made active
+//when game is opened, these features are made active
 window.onload = function () {
   $(".show").hide();
   $("#stop").on("click", stop);
@@ -19,85 +19,92 @@ window.onload = function () {
 };
 
 //questions display one at a time; "click" indicates answer and moves to "function showAnswer" /
-var myquest = [ {
+var myquest = [{
   question: "1. In the Golden Age of Dutch Painting, still life imagery was:",
-  answer: ["representation the actual item shown","religious allergories","political commentary"],
-  correctAnswer: " " + "religious allergories",
+  answer: ["representation the actual item shown", "religious allegories", "political commentary"],
+  correctAnswer: "religious allegories",
   urlImage: "https://media.giphy.com/media/Kxqyq5Q486NVu/giphy.gif"
 },
 {
   question: "2. Jacob Lawrence is best known for a series of paintings about:",
-  answer: ["The Great Migration","The Trail of Tears","Portraits of Abolitionists"],
-  correctAnswer: " " + "The Great Migration",
+  answer: ["The Great Migration", "The Trail of Tears", "Portraits of Abolitionists"],
+  correctAnswer: "The Great Migration",
   urlImage: "https://media.giphy.com/media/l4FB5dgbc21nQPKhy/giphy.gif"
 },
 {
   question: "3. The Eiffel Tower scandalized Paris when it was built because:",
-  answer: ["it resembled a woman spreading her skirts","it didn't match the city's existing architectural style","it was built over a Roman-era burial ground"],
-  correctAnswer: " " + "it resembled a woman spreading her skirts",
+  answer: ["it resembled a woman spreading her skirts", "it didn't match the city's existing architectural style", "it was built over a Roman-era burial ground"],
+  correctAnswer: "it resembled a woman spreading her skirts",
   urlImage: "https://media.giphy.com/media/4cdJGWnSOBqYo/giphy.gif"
 },
 {
   question: "4. The &ldquo;Mother of American Modernism&rdquo; is:",
-  answer: ["Gertrude Stein","Georgia O'Keeffe","Diane Arbus"],
-  correctAnswer: " " + "Georgia O'Keeffe",
+  answer: ["Gertrude Stein", "Georgia O'Keeffe", "Diane Arbus"],
+  correctAnswer: "Georgia O'Keeffe",
   urlImage: "https://media.giphy.com/media/3ornjHBJr0NzgqmFSo/giphy.gif"
 },
 {
   question: "5. Sculptures looted from the Acropolis are in which museum:",
-  answer: ["The British Museum, London","The Museum of Cycladic Art, Athens","The Hermitage Museum, St. Petersburg"],
+  answer: ["The British Museum, London", "The Museum of Cycladic Art, Athens", "The Hermitage Museum, St. Petersburg"],
   correctAnswer: "The British Museum, London",
-  urlImage: " " + "https://media.giphy.com/media/U8DnMoBzvHSt1AxSkV/giphy.gif"
+  urlImage: "https://media.giphy.com/media/U8DnMoBzvHSt1AxSkV/giphy.gif"
 },
 {
   question: "6. During WWII, contemporary art was confiscated and destroyed, because it was classified as:",
-  answer: ["angry","without monetary value","degenerate"],
+  answer: ["angry", "without monetary value", "degenerate"],
   correctAnswer: "degenerate",
-  urlImage: " " + "https://media.giphy.com/media/24FMod5txpeBynO2eS/giphy.gif"
+  urlImage: "https://media.giphy.com/media/24FMod5txpeBynO2eS/giphy.gif"
 },
 {
   question: "7. The artist, Frieda Kahlo, was married to:",
-  answer: ["Leon Trotsky","Diego Rivera","Rufino Tamayo"],
+  answer: ["Leon Trotsky", "Diego Rivera", "Rufino Tamayo"],
   correctAnswer: "Diego Rivera",
-  urlImage: " " + "https://media.giphy.com/media/Wxlx4kVDZ7IZsLBhDH/giphy.gif"
+  urlImage: "https://media.giphy.com/media/Wxlx4kVDZ7IZsLBhDH/giphy.gif"
 },
 {
   question: "8. Most western-European artists found their benefactors in the form of:",
-  answer: ["the Catholic Church","private comissions by wealthy businessmen","the monarchy"],
+  answer: ["the Catholic Church", "private comissions by wealthy businessmen", "the monarchy"],
   correctAnswer: "the monarchy",
-  urlImage: " " + "https://media.giphy.com/media/Bgxm9NBY96h0s/giphy.gif"
+  urlImage: "https://media.giphy.com/media/Bgxm9NBY96h0s/giphy.gif"
 },
 {
   question: "9. The first comprehensive photographic documentation of the Dead Sea Scrolls was commissioned on behalf of:",
-  answer: ["Claremont College","Oxford University","Harvard Divinity School"],
+  answer: ["Claremont College", "Oxford University", "Harvard Divinity School"],
   correctAnswer: "Claremont College",
-  urlImage: " " + "https://media.giphy.com/media/ieNgLZAiId0uA/giphy.gif"
+  urlImage: "https://media.giphy.com/media/ieNgLZAiId0uA/giphy.gif"
 },
 {
   question: "10. Printing in Asia is known to have existed as early as:",
-  answer: ["The Goryeo Dynasty in Korea","The Han Dynasty in China","The Edo Period in Japan"],
-  correctAnswer: "The Han Dynasty in China", 
-  urlImage: " " + "https://media.giphy.com/media/rHR8qP1mC5V3G/giphy.gif"
-} ];
+  answer: ["The Goryeo Dynasty in Korea", "The Han Dynasty in China", "The Edo Period in Japan"],
+  correctAnswer: "The Han Dynasty in China",
+  urlImage: "https://media.giphy.com/media/rHR8qP1mC5V3G/giphy.gif"
+}];
 
 //button displays answer and gif for entertainment value in hidden container "show" behind "gamecontainer"
-function showAnswer(){
-  $("#display-answer").empty();
-  $("#display-answer").append("<b>"+myquest[indexOfmyquest].correctAnswer.toUpperCase())
-  $("#answerGif").attr("src",myquest[indexOfmyquest].urlImage)
+function showAnswer() {
+  $("#display-answer").empty();//cleans previous answers
+  $("#display-answer").append("<b>" + myquest[indexOfmyquest].correctAnswer.toUpperCase())
+  $("#answerGif").attr("src", myquest[indexOfmyquest].urlImage)
   $(".gamecontainer").hide()
   $(".show").show()
 }
 
 //after displaying correct answer and gif, hide container and return to "game container"
-function returnToGame(){
+function returnToGame() {
   $(".gamecontainer").show()
   $(".show").hide()
+  indexOfmyquest++;
+  showquestions()
 
+  //when reaching index of last item of array, show results
+  if (indexOfmyquest === myquest.length) {
+    clearInterval(intervalId); //stops clock
+    showresults()//shows score
+  }
 }
 
 //when showing next question in array, clear timer to allow 10 seconds for player input
-function showquestions () {
+function showquestions() {
   $("#questions").empty()
   $("#answers").empty()
   $("#next").on("click", showAnswer).show();
@@ -114,76 +121,95 @@ function showquestions () {
 
   //registering and storing answers with radio button clicks
   else {
-    $("#questions").html(myquest[ indexOfmyquest ].question)
-    for (var indexA = 0; indexA < myquest[ indexOfmyquest ].answer.length; indexA++) {
-      $("#answers").append("<input class='response' type='radio' name=" + indexOfmyquest + " value=" + indexA + ">" + myquest[ indexOfmyquest ].answer[ indexA ] +"<br>")
+    $("#questions").html(myquest[indexOfmyquest].question)
+    for (var indexA = 0; indexA < myquest[indexOfmyquest].answer.length; indexA++) {
+      $("#answers").append("<input class='response' type='radio' name=" + indexOfmyquest + " value=" + indexA + " data-answer='" + myquest[indexOfmyquest].answer[indexA] + "'>" + myquest[indexOfmyquest].answer[indexA] + "<br>")
     }
 
-    //what do do when a radio button is clicked
+    //instructions for radio button clicks
     $(".response").on("click", function () {
-      var res = $("#answers").children("input:checked").val()
-      //playerAnswer.push(res)
-      console.log("clicked radio button: ", res)
+      var res = $(this).attr("data-answer")
       showAnswer()
       $("#next").on("click", showAnswer).show();
+      
 
-      //if correct, add a point to correct answers
-      if (parseInt(myquest[ indexOfmyquest ].correctAnswer) === parseInt(res)) {
-        console.log("c: ", (myquest[ indexOfmyquest ].correctAnswer), (parseInt(res)))
+      //if correct, add a point to correct answers and pause timer, show gif
+      if ((myquest[indexOfmyquest].correctAnswer).toLowerCase() === (res.toLowerCase())) {
+        console.log("c: ", (myquest[indexOfmyquest].correctAnswer), (parseInt(res)))
         correctAnswers++
+        $("#questions").empty()
+        $("#answers").empty()
+        $("#next").on("click", showAnswer).show();
+        clearInterval(intervalId);
+        time = 10;
+        $("#time").text("00:10");
+        intervalId = setInterval(count, 1000);
       }
-      //if incorrect, add a point to incorrect answers
+       
+      //if incorrect, add a point to incorrect answers and pause timer, show gif
       else {
-        console.log("i: ", (myquest[ indexOfmyquest ].correctAnswer), (parseInt(res)))
+        console.log("i: ", (myquest[indexOfmyquest].correctAnswer), (parseInt(res)))
         incorrectGuesses++
+        $("#questions").empty()
+        $("#answers").empty()
+        $("#next").on("click", showAnswer).show();
+        clearInterval(intervalId);
+        time = 10;
+        $("#time").text("00:10");
+        intervalId = setInterval(count, 1000);
       }
-      //need to define what to do if timer runs out without radio button being clicked--> "var skipped"
+      
       //loading successive questions to be displayed at each click event
-      indexOfmyquest++
-      showquestions()
       $("#next").on("click", showAnswer).show()
     })
   }
 }
 
 //show score at end of game
-function showresults () {
+function showresults() {
   console.log(correctAnswers, incorrectGuesses)
 
-  //where to find data to populate results
-  $("#questions").empty()
-  $("#answers").empty()
+  //where to populate results
+  $("#results").empty()
 
-  //html on the fly: statement to declare results
+  //changing visibility to declare results
   $(".gamecontainer").hide()
   $(".scorecard").show()
-  $("#questions").append("<h3>Your results:</h3>")
 
-  var unanswered = myquest.length - correctAnswers - incorrectGuesses
-  console.log("unanswered: ", unanswered)
-  $("#questions").append("<p>Correct: <span>" + correctAnswers + "</span></p>")
-  $("#questions").append("<p>Incorrect: <span>" + incorrectGuesses + "</span></p>")
-  $("#questions").append("<p>Unanswered: <span>" + unanswered + "</span></p>")
+  //total score cannot exceed number of questions
+  unansweredQ=myquest.length;
+  var unansweredResults=unansweredQ-correctAnswers-incorrectGuesses
+  
+  //html on the fly: statement to declare results
+  $("#results").append("<h3>Your results:</h3>")
+  $("#results").append("<p>Correct: <span>" + correctAnswers + "</span></p>")
+  $("#results").append("<p>Incorrect: <span>" + incorrectGuesses + "</span></p>")
+  $("#results").append("<p>Unanswered: <span>" + unansweredResults + "</span></p>")
 }
-//timer thingies
-function count () {
+//timer-related thingies
+function count() {
   //timer decrementing
   time--;
 
-  //advance to next quetion if time runs out
+  //advance to answer if time runs out
   if (time === 0) {
-    indexOfmyquest++
-    showquestions()
+
+    showAnswer();
+  unansweredQ++;
+  console.log("unanswered:"+unansweredQ);
+
+//show score after last question/answer dsiplay.
+  if (indexOfmyquest === myquest.length) {
+  showresults()
+  }
   }
   //how to display descending timer
   var converted = timeConverter(time);
   $("#time").text(converted);
-
-  //wait for "PROCEED" click to begin countdown again: clearInterval to reset with each new question
 }
 
 //defining timer format
-function timeConverter (t) {
+function timeConverter(t) {
   var minutes = Math.floor(t / 60);
   var seconds = t - (minutes * 60);
 
@@ -207,23 +233,24 @@ function timeConverter (t) {
 }
 
 //when hitting BEGIN! button to start and/or reloading the game (and clicking BEGIN button), reset timer, scores, array of questions
-function start () {
+function start() {
   reset()
   showquestions()
 }
 
 // when hitting STOP! button to pause and/or end the game, stop timer count and show current results
-function stop () {
+function stop() {
   clearInterval(intervalId);
   showresults()
 }
 
 //"reset" function that is part of "start" function
-function reset(){
-  time=10;
+function reset() {
+  time = 10;
   indexOfmyquest = 0;
   correctAnswers = 0;
   incorrectGuesses = 0;
+  unansweredQ = 0;
   var converted = timeConverter(time);
   $("#time").text(converted)
 }
